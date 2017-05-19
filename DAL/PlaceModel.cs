97b288f51bc8942b2a25f52e74b4d7b057c870ff
bohdanka194 <bohdanka194@gmail.com>
2017-05-19 19:34:08 +0300

    Architecture models

---
+++ DAL/PlaceModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    class PlaceModel
+    {
+        public int ID { get; set; }
+        public string City { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int CountRow { get; set; }
+        public int CountSeatInRow { get; set; }
+    }
+}
---
+++ DAL/TicketModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class TicketModel
+    {
+        public int ID { get; set; }
+        public int ConcertID { get; set; }
+        public int? UserID { get; set; }
+        public int Price { get; set; }
+        public int Row { get; set; }
+        public int Seat { get; set; }
+        
+       
+    }
+}
